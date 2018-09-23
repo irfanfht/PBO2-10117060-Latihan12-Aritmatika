4a2c5e2841c624312412dfcc67630ccf7d23523e
kekerofficial <43506956+kekerofficial@users.noreply.github.com>
2018-09-23 12:32:44 +0700

    Add files via upload

---
+++ src/pbo2/pkg10117060/latihan12/aritmatika/PBO210117060Latihan12Aritmatika.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117060.latihan12.aritmatika;
+
+/**
+ *
+* @author 
+ * NAMA : Irfan Faisal Hutama Tanjung.
+ * KELAS : PBO2
+ * NIM : 10117060
+ * Deskripsi Program : Program ini berisi operasi aritmatika. yang dimana
+ * a, b, dan c itu sebagai variabel yang memiliki nilai masing2.
+ */
+public class PBO210117060Latihan12Aritmatika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 25;
+        int d = 25;
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("b / a = " + (b / a));
+        System.out.println("b % a = " + (b % a));
+        System.out.println("c % a = " + (c % a));
+        System.out.println("a++ = " + (a++));
+        System.out.println("b-- = " + (a--));
+        System.out.println("d++ = " + (d++));
+        System.out.println("++d = " + (++d));
+    }
+    
+}
